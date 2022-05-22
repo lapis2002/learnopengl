e64246bba1c885849814451edc1cd76c4b0c81a0
Hang <irene.duong799@gmail.com>
2022-05-22 12:30:21 -0700

    Hello Triangle

--- main.cpp
+++ main.cpp
@@ -5,9 +5,37 @@
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+void checkCompileShader(unsigned int shader);
+void checkCompileProgram(unsigned int program);
 
 const unsigned int WIDTH = 800;
 const unsigned int HEIGHT = 600;
+
+float vertices[] = {
+     0.5f,  0.5f, 0.0f,  // top right
+     0.5f, -0.5f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f,  // bottom left
+    -0.5f,  0.5f, 0.0f   // top left 
+};
+
+unsigned int indices[] = {  // note that we start from 0!
+    0, 1, 3,   // first triangle
+    1, 2, 3    // second triangle
+};
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0";
 
 int main() {
     glfwInit();
@@ -34,6 +62,79 @@ int main() {
 
     glViewport(0, 0, WIDTH, HEIGHT);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // SHADER
+    //create vertex shader
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+    // attach shader source code to the shader object and compile the shader
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    checkCompileShader(vertexShader);
+
+    // create fragment shader
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();  // ID reference
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    checkCompileProgram(shaderProgram);
+
+    glUseProgram(shaderProgram);
+
+    // delete shader after use
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    unsigned int VBO, VAO, EBO;
+    // subsequent vertex attribute calls from that point on will be stored inside the VAO
+    /*
+    VAO stores:
+        Calls to glEnableVertexAttribArray or glDisableVertexAttribArray.
+        Vertex attribute configurations via glVertexAttribPointer.
+        Vertex buffer objects associated with vertex attributes by calls to glVertexAttribPointer.
+    */
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    // bind Vertex Array Object
+    glBindVertexArray(VAO);
+
+    // copy vertices array in buffer for OpenGL to use
+    // OpenGL allows us to bind to several buffers at once as long as they have a different buffer type. 
+    /*
+    GL_STREAM_DRAW: the data is set only once and used by the GPU at most a few times.
+    GL_STATIC_DRAW: the data is set only once and used many times.
+    GL_DYNAMIC_DRAW: the data is changed a lot and used many times.
+    */
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);     // !buffer type of a vertex buffer
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);      // copy user-defined data into the currently bound buffer
+
+    //  bind the corresponding EBO each time rendering an object with indices
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);    // indices
+
+    // set the vertex attribute pointer
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    //!!  NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+    
+    // unbind VAO
+    glBindVertexArray(0);
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
 
@@ -41,6 +142,16 @@ int main() {
         // clear the color buffer, the entire color buffer will be filled with the color as configured by glClearColor. 
         glClearColor(0.2f, 0.3f, 0.3f, 0.1f);   // !state-setting function
         glClear(GL_COLOR_BUFFER_BIT);           // !state-using function
+
+        // use shader program to render object
+        glUseProgram(shaderProgram);
+
+        // draw the object
+        // bind the VAO with the preferred settings before drawing the object 
+        glBindVertexArray(VAO);
+        
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);    // draw using indices specified in EBO
 
         // swap the color buffer
         // swap the back buffer to the front buffer so the image can be displayed without still being rendered to, 
@@ -52,6 +163,12 @@ int main() {
         // calls the corresponding functions (can be registered via callback methods)
         glfwPollEvents();
     }
+
+    //  de-allocate all resources
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
 
     glfwTerminate();    
     return 0;
@@ -65,4 +182,28 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 // -------------------------------------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)  glfwSetWindowShouldClose(window, true);
+}
+
+// -------------------------------------------------------------------------------------------------------------------------------------
+void checkCompileShader(unsigned int shader) {
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+}
+
+// -------------------------------------------------------------------------------------------------------------------------------------
+void checkCompileProgram(unsigned int program) {
+    int success;
+    char infoLog[512];
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+
+    if (!success) {
+        glGetProgramInfoLog(program, 512, NULL, infoLog);
+        std::cout << "ERROR::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
 }
